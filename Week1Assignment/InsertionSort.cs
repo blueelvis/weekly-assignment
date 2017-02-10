4b23e3120f89684a43fb9e9e9419468c92201805
Pranav V Jituri <blueelvisrocks@gmail.com>
2017-02-10 12:44:28 +0530

    Array Initialization based on user requirements

---
+++ Week1Assignment/InsertionSort.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Week1Assignment
+{
+    class InsertionSort
+    {
+        static void Main(string[] args)
+        {
+            int numberOfElements;
+            Console.WriteLine("Program to sort numbers using Insertion Sort\n\n");
+            Console.WriteLine("Enter the number of elements to sort = \t");
+            numberOfElements = int.Parse(Console.ReadLine());
+
+            int[] userElements = new int[numberOfElements];
+
+            for (int i = 0; i < userElements.Length; i++)
+            {
+                userElements[i] = int.Parse(Console.ReadLine());
+            }
+        }
+    }
+}
